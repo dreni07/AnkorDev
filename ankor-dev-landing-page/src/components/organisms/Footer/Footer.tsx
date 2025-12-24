1841c7a86f9884366a202ba9b5e19fa062b09617
Adem <devloom45@gmail.com>
2025-12-25 00:40:44 +0100

    uptade i footerit
    uptade footer

--- ankor-dev-landing-page/src/components/organisms/Footer/Footer.tsx
+++ ankor-dev-landing-page/src/components/organisms/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { companyConfig } from '../../../config/company.config';
 import { Text } from '../../atoms/Text';
+import { FaTwitter, FaLinkedin, FaGithub, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import './Footer.css';
 
 export const Footer: React.FC = () => {
@@ -20,9 +21,31 @@ export const Footer: React.FC = () => {
           <div className="footer__links">
             <div className="footer__links-group">
               <Text variant="label" className="footer__links-title">
-                Legal
+                Company
               </Text>
-              {companyConfig.footer.legal.map((link) => (
+              {companyConfig.footer.company.map((link) => (
+                <a key={link.id} href={link.href} className="footer__link">
+                  <Text variant="small">{link.label}</Text>
+                </a>
+              ))}
+            </div>
+
+            <div className="footer__links-group">
+              <Text variant="label" className="footer__links-title">
+                Software
+              </Text>
+              {companyConfig.footer.software.map((link) => (
+                <a key={link.id} href={link.href} className="footer__link">
+                  <Text variant="small">{link.label}</Text>
+                </a>
+              ))}
+            </div>
+
+            <div className="footer__links-group">
+              <Text variant="label" className="footer__links-title">
+                Contact
+              </Text>
+              {companyConfig.footer.contact.map((link) => (
                 <a key={link.id} href={link.href} className="footer__link">
                   <Text variant="small">{link.label}</Text>
                 </a>
@@ -33,16 +56,50 @@ export const Footer: React.FC = () => {
               <Text variant="label" className="footer__links-title">
                 Social
               </Text>
-              {companyConfig.footer.socialLinks.map((link) => (
-                <a key={link.id} href={link.href} className="footer__link">
-                  <Text variant="small">{link.label}</Text>
-                </a>
-              ))}
+              {companyConfig.footer.socialLinks.map((link) => {
+                const getIcon = (id: string) => {
+                  switch (id.toLowerCase()) {
+                    case 'twitter':
+                      return <FaTwitter className="footer__social-icon" />;
+                    case 'linkedin':
+                      return <FaLinkedin className="footer__social-icon" />;
+                    case 'github':
+                      return <FaGithub className="footer__social-icon" />;
+                    case 'facebook':
+                      return <FaFacebook className="footer__social-icon" />;
+                    case 'instagram':
+                      return <FaInstagram className="footer__social-icon" />;
+                    case 'youtube':
+                      return <FaYoutube className="footer__social-icon" />;
+                    default:
+                      return null;
+                  }
+                };
+
+                return (
+                  <a key={link.id} href={link.href} className="footer__link footer__social-link">
+                    {getIcon(link.id)}
+                    <Text variant="small">{link.label}</Text>
+                  </a>
+                );
+              })}
             </div>
           </div>
         </div>
 
         <div className="footer__bottom">
+          <div className="footer__bottom-left">
+            <Text variant="h4" className="footer__bottom-logo">
+              {companyConfig.logo}
+            </Text>
+            <div className="footer__legal-links">
+              {companyConfig.footer.legal.map((link) => (
+                <a key={link.id} href={link.href} className="footer__legal-link">
+                  <Text variant="small">{link.label}</Text>
+                </a>
+              ))}
+            </div>
+          </div>
           <Text variant="small" className="footer__copyright">
             © {new Date().getFullYear()} {companyConfig.name}. All rights reserved.
           </Text>
